4c6b3e2b7dd0ebaedd83ee81b3bf93e1d84ec2b8
nlord <natalielordd@gmail.com>
2026-02-19 14:57:09 -0800

    Placeholder files for testing

---
+++ terraform_provider/provider.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// *** Template implementation for testing purposes 
+
+var _ provider.Provider = (*Provider)(nil)
+
+func newProvider() provider.Provider {
+	return &Provider{}
+}
+
+type Provider struct{}
+
+type providerModel struct {
+	Host     types.String `tfsdk:"host"`
+	Username types.String `tfsdk:"username"`
+	Password types.String `tfsdk:"password"`
+	Port     types.Int64  `tfsdk:"port"`
+	SshKey   types.String `tfsdk:"sshkey"`
+}
+
+func (p *Provider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "junos-vqfx-evpn-vxlan"
+}
+
+func (p *Provider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"host": schema.StringAttribute{
+				Required: true,
+			},
+			"username": schema.StringAttribute{
+				Required: true,
+			},
+			"password": schema.StringAttribute{
+				Optional:  true,
+				Sensitive: true,
+			},
+			"port": schema.Int64Attribute{
+				Required: true,
+			},
+			"sshkey": schema.StringAttribute{
+				Optional:  true,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func (p *Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var config providerModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (p *Provider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return nil
+}
+
+func (p *Provider) Resources(_ context.Context) []func() resource.Resource {
+	return nil
+}
